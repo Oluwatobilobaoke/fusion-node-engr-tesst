2a6ff2c186b4d52587f7c1af13175267e1e6bc60
Ibrahim Adekunle <adefemi101@gmail.com>
2022-06-29 12:34:25 +0100

    fix PORT issues

--- src/db/models/Transaction.ts
+++ src/db/models/Transaction.ts
@@ -1,10 +1,10 @@
-import { DataTypes, Model, ModelStatic, Optional } from 'sequelize'
-import sequelizeConnection from '../config'
-import { Transaction as TransactionInterface } from '../../api/interfaces'
+import { DataTypes, Model, ModelStatic, Optional } from "sequelize";
+import sequelizeConnection from "../config";
+import { Transaction as TransactionInterface } from "../../api/interfaces";
 
 enum Txn_Type {
   debit = "debit",
-  credit = "credit"
+  credit = "credit",
 }
 
 // export enum Purpose {
@@ -14,72 +14,76 @@ enum Txn_Type {
 // }
 
 interface TransactionMetadata {
-  info: string | null
+  info: string | null;
 }
 
 interface TransactionAttributes {
-  id: number
-  txn_type?: Txn_Type
-  purpose?: string
-  amount: number
-  account_id: number
-  reference: string
-  balance_before?: number
-  balance_after?: number
-  metadata: TransactionMetadata | null
-  createdAt?: Date
-  updatedAt?: Date
-  deletedAt?: Date
+  id: number;
+  txn_type?: Txn_Type;
+  purpose?: string;
+  amount: number;
+  account_id: number;
+  reference: string;
+  balance_before?: number;
+  balance_after?: number;
+  metadata: TransactionMetadata | null;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
 }
 
-export interface TransactionInput extends Optional<TransactionAttributes, 'id' > {}
+export interface TransactionInput
+  extends Optional<TransactionAttributes, "id"> {}
 
 export interface TransactionOutput {
   success: boolean;
   message?: string;
-  data: TransactionInterface
+  data: TransactionInterface;
 }
 
-class Transaction extends Model<TransactionAttributes, TransactionInput> implements TransactionAttributes {
-    public id!: number
-    public txn_type!: Txn_Type
-    public purpose!: string
-    public amount!: number
-    public account_id!: number
-    public reference!: string
-    public balance_before!: number
-    public balance_after!: number
-    public metadata!: TransactionMetadata | null
+class Transaction
+  extends Model<TransactionAttributes, TransactionInput>
+  implements TransactionAttributes
+{
+  public id!: number;
+  public txn_type!: Txn_Type;
+  public purpose!: string;
+  public amount!: number;
+  public account_id!: number;
+  public reference!: string;
+  public balance_before!: number;
+  public balance_after!: number;
+  public metadata!: TransactionMetadata | null;
 
-    
-    // timestamps!
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
-    public readonly deletedAt!: Date;
+  // timestamps!
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+  public readonly deletedAt!: Date;
 }
 
-Transaction.init({
+Transaction.init(
+  {
     id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
     },
     txn_type: {
       type: DataTypes.ENUM("debit", "credit"),
-      allowNull: false
+      allowNull: false,
     },
     purpose: {
-      type: DataTypes.ENUM('deposit', 'transfer', 'reversal'),
-      allowNull: false    
+      type: DataTypes.ENUM("deposit", "transfer", "reversal"),
+      allowNull: false,
     },
     amount: {
       type: DataTypes.DECIMAL(20, 4).UNSIGNED,
       allowNull: false,
     },
-    account_id:{
+    account_id: {
       type: DataTypes.INTEGER.UNSIGNED,
-      allowNull: false
+      allowNull: false,
     },
     reference: {
       type: DataTypes.STRING,
@@ -92,9 +96,9 @@ Transaction.init({
       type: DataTypes.DECIMAL(20, 4).UNSIGNED,
       allowNull: false,
     },
-    metadata:{
-      type: DataTypes.TEXT,
-    }, 
+    metadata: {
+      type: DataTypes.STRING,
+    },
     createdAt: {
       type: DataTypes.DATE,
       allowNull: false,
@@ -109,12 +113,12 @@ Transaction.init({
       type: DataTypes.DATE,
       allowNull: true,
     },
-}, {
-  timestamps: true,
-  sequelize: sequelizeConnection,
-  paranoid: true
-})
+  },
+  {
+    timestamps: true,
+    sequelize: sequelizeConnection,
+    paranoid: true,
+  }
+);
 
-
-
-export default Transaction
+export default Transaction;
