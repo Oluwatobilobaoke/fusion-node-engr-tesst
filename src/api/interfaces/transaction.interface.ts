dff90fd73cd6fe48ed7c899472dfeed2f55bc067
Ibrahim Adekunle <adefemi101@gmail.com>
2022-06-29 12:54:11 +0100

    fix PORT issues

--- src/api/interfaces/transaction.interface.ts
+++ src/api/interfaces/transaction.interface.ts
@@ -1,25 +1,23 @@
 export interface Transaction {
-  id?: number
-  txn_type?: Txn_Type
-  purpose?: string
-  amount?: number
-  account_id?: number
-  reference?: string
-  balance_before?: number
-  balance_after?: number
-  metadata?: TransactionMetadata | null
-  createdAt?: Date
-  updatedAt?: Date
-  deletedAt?: Date | null
+  id?: number;
+  txn_type?: Txn_Type;
+  purpose?: string;
+  amount?: number;
+  account_id?: number;
+  reference?: string;
+  balance_before?: number;
+  balance_after?: number;
+  metadata?: string | null;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date | null;
 }
 
 export enum Txn_Type {
   debit = "debit",
-  credit = "credit"
+  credit = "credit",
 }
 
-
-
 export interface TransactionMetadata {
-  info: string | null
+  info: string | null;
 }
